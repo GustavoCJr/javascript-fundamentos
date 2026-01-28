f934b2c37bc31634bd4c04f1f75035d228db2293
Gustavo Cassettari <cassettarigustavo476@gmail.com>
2026-01-28 00:24:03 -0300

    feat: adiciona o arquivo 05-nomalizador-usuario

---
+++ 04-objetos/01-objetos-fundamentos/05-nomalizador-usuario.js
@@ -0,0 +1,21 @@
+//Exercício 5 — Normalizador de usuário (objetos aninhados)
+const usuario = {
+  nome: "GUSTAVO",
+  endereco: {
+    cidade: "São Paulo",
+    uf: "SP",
+  },
+  skills: ["JavaScript", "Python", "Java", "C"],
+};
+function normalizarUsuario(usuario) {
+  return {
+    nome: usuario.nome.toUpperCase(),
+    cidade:
+      usuario.endereco.cidade[0].toUpperCase() +
+      usuario.endereco.cidade.slice(1),
+    uf: usuario.endereco.uf.toUpperCase(),
+    totalSkills: usuario.skills.lenght,
+  };
+}
+const resultado = normalizarUsuario(usuario);
+console.log(resultado);
